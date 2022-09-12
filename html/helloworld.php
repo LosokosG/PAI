e583646b3fc6a682e87993ec4deaf246518cc979
LosokosG <losokos@gmail.com>
2022-09-12 12:51:17 +0200

    1

---
+++ html/helloworld.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+   <meta charset="utf-8"> 
+</head>
+<body bgcolor="teal" text="white">
+    <p> Hello World! </p>
+</body>
+
+
+
+<?php
+
+$counter = 1;
+
+echo "Hello World! - PHP <br>" ;
+echo "Hello friend <br>";
+echo "My counter $counter <br>"; 
+
+?>
